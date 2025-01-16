c57209656ed600fb4c4f20dfe6bff0a6f92dde09
iitech385 <iitech385@gmail.com>
2025-01-16 06:23:22 +0400

    Fix VerifyEmail CSS import path

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/signup.css';
+import './SignUp.css';
 
 interface VerifyEmailProps {
   email: string;
